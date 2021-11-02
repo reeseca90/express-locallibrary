e46a241fa810745d17f707933917300e062c22e3
Craig <reeseca90@gmail.com>
2021-11-02 14:38:27 -0400

    more files weren't being tracked

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/cool', function(req, res, next) {
-  res.send('cool');
-});
-
 module.exports = router;
