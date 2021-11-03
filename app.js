a07a51d0fffaf6f192a469132e4eb61fbfe6089b
Craig <reeseca90@gmail.com>
2021-11-03 14:49:27 -0400

    removed DB information from app.js for use as env variable on heroku

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://craig:libraryapp@cluster0.jqdmn.mongodb.net/local_library?retryWrites=true&w=majority';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
