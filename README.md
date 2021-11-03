41d892014f16bf1cd6597b87fd550b0735d9edc1
Craig <reeseca90@gmail.com>
2021-11-03 14:40:18 -0400

    deployment prep

---
+++ README.md
@@ -0,0 +1,5 @@
+# express-locallibrary
+
+Program that tracks a library collection: Books and their associated Authors, Genres, and copies (book instances)
+
+Runs using Express/MongoDB
--- app.js
+++ app.js
@@ -17,7 +17,8 @@ app.use(helmet());
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://craig:libraryapp@cluster0.jqdmn.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://craig:libraryapp@cluster0.jqdmn.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
